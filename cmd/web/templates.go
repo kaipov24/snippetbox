bbce34942a0aa12e73440becfb92ec80085fe748
kaipov24 <kaipovich24@gmail.com>
2025-06-21 19:49:11 +0600

    Latest

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/kaipov24/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
